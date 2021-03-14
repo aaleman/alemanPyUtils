9ca63d90ba4e29b518cf1e2ded8937c2a745aa14
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2021-03-14 11:31:47 +0100

    Fixed test_runner

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -5,16 +5,17 @@ from alemanpyutils.io.Runner import Runner
 from alemanpyutils.io.RunnerConfig import RunnerConfig
 from alemanpyutils.io.Writer import Writer
 from alemanpyutils.io.readers.RandomStringDataReader import RandomStringDataReader
-from alemanpyutils.io.writers.FileDataWriter import FileDataWriter
+from alemanpyutils.io.writers.StandardOutputWriter import StandardOutputWriter
 
 
 def test_runner():
 
     rc = RunnerConfig(batch_size=10, num_processors=10)
 
-    reader = Reader(data_reader=RandomStringDataReader(5, 100), runner_config=rc)
+    reader = Reader(data_reader=RandomStringDataReader(
+        5, 100), runner_config=rc)
 
-    writer = Writer(data_writer=FileDataWriter("/tmp/a/out.txt"), runner_config=rc)
+    writer = Writer(data_writer=StandardOutputWriter(), runner_config=rc)
 
     processors = []
     for i in range(0, rc.num_processors):
