4c4f378efe7d2e2eb62492826cfb9d9c0a8a6a3c
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2021-03-25 22:24:03 +0100

    Updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='alemanpyutils',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='alemanpyutils',
     author='Alejandro Alemán',
     license='',
@@ -14,3 +14,4 @@ setup(
 
 
 )
+
